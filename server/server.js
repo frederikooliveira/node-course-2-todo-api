4ccc40ae4a2e25d2aa60324dd4dc6a9cb1187948
Frederiko Oliveira de Sousa <frederiko.sousa@globaltec.com.br>
2018-02-08 23:06:17 -0200

    add GET todos route

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 
@@ -15,15 +15,23 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
 
-    todo.save().then((doc) =>{
+    todo.save().then((doc) => {
         res.send(doc);
-    }, (e)=>{
+    }, (e) => {
         res.status(400).send(e);
     })
 });
 
-app.listen(3000, () =>{
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.listen(3000, () => {
     console.log('Started on port 3000')
 })
 
-module.exports = {app}
+module.exports = { app }
