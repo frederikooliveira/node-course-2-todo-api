a4b400755402941881e8c2c286b9945ef0045565
Frederiko Oliveira de Sousa <frederiko.sousa@globaltec.com.br>
2018-02-16 22:44:11 -0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,35 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs') ;
 
-var data = {
-    id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt)=>{
+//     bcrypt.hash(password, salt,(err, hash)=>{
+//         console.log(hash);
+//     })
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded)
+var hashedPassword = '$2a$10$sMjqvDuE4ucM1UDXoQ4MkOj527nH74NgML57jqgo3uKEoCW78fKUy';
+
+bcrypt.compare(password, hashedPassword, (err, res)=>{
+    console.log(res);
+})
+
+
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded)
+
+
+
 
 // var message = 'I am user numer 3';
 // var hash = SHA256(message).toString();
