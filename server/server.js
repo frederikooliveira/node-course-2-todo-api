67fabb06af64a22953cb245487c581fd85db727f
Frederiko Oliveira de Sousa <frederiko.sousa@globaltec.com.br>
2018-02-18 11:16:24 -0300

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -106,6 +106,11 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+})
+
+
 app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
 
@@ -118,9 +123,13 @@ app.post('/users/login', (req, res) => {
     })
 })
 
-app.get('/users/me', authenticate, (req, res) => {
-    res.send(req.user);
-})
+app.delete('/users/me/token', authenticate, (req, res)=>{
+    req.user.removeToken(req.token).then(()=>{
+        res.send();
+    }, ()=>{
+        res.status(400).send();
+    })
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`)
